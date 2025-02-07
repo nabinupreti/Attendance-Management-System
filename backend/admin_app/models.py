d5135c6b1dc3b4b90a31c8fa86e2a24ea9762658
sahansharma <sahansharma1000@gmail.com>
2025-02-07 19:11:18 +0545

    Cleaned the codebase and maintained the file structure.

--- backend/admin_app/models.py
+++ backend/admin_app/models.py
@@ -9,14 +9,14 @@ class AdminUser(AbstractUser):
 
     groups = models.ManyToManyField(
         Group,
-        related_name='admin_app_user_set',  # Add a unique related_name
+        related_name='admin_app_user_set',
         blank=True,
         help_text='The groups this user belongs to.',
         verbose_name='groups',
     )
     user_permissions = models.ManyToManyField(
         Permission,
-        related_name='admin_app_user_permissions_set',  # Add a unique related_name
+        related_name='admin_app_user_permissions_set',
         blank=True,
         help_text='Specific permissions for this user.',
         verbose_name='user permissions',
